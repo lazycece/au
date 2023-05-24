dbdc27637af59d350b488dec16899aae5f4b5ab3
lazycece <lazycece@gmail.com>
2023-05-24 22:10:16 +0800

    fix

--- au-core/src/main/java/com/lazycece/au/DefaultFilterFactory.java
+++ au-core/src/main/java/com/lazycece/au/DefaultFilterFactory.java
@@ -2,6 +2,8 @@ package com.lazycece.au;
 
 import com.lazycece.au.filter.AuFilter;
 
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * @author lazycece
  * @date 2019/10/03
@@ -9,7 +11,7 @@ import com.lazycece.au.filter.AuFilter;
 public class DefaultFilterFactory implements FilterFactory {
 
     @Override
-    public AuFilter newInstance(Class<? extends AuFilter> clazz) throws InstantiationException, IllegalAccessException {
-        return clazz.newInstance();
+    public AuFilter newInstance(Class<? extends AuFilter> clazz) throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        return clazz.getDeclaredConstructor().newInstance();
     }
 }
